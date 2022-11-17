0c5d6fa07946196360b7df68d5bcd4e40ef4d955
Money Maker <mitchell.harris@westnet.com.au>
2022-11-17 18:58:03 +0800

    setting up files

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,6 @@
+// this page sends users to the login page when 'withAuth' 
+// is called
+
 const withAuth = (req, res, next) => {
     // If the user is not logged in, redirect the request to the login route
     if (!req.session.logged_in) {
