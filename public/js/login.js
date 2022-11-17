a284e9a179adf117286b93e3cd3f4bdd9d27c679
Money Maker <mitchell.harris@westnet.com.au>
2022-11-17 20:57:25 +0800

    added controllers code and models code

--- public/js/login.js
+++ public/js/login.js
@@ -2,6 +2,7 @@ async function loginFormHandler(event) {
     event.preventDefault();
   
     const username = document.querySelector("#username-login").value.trim();
+    const email = document.querySelector("#email-login").value.trim();
     const password = document.querySelector("#password-login").value.trim();
   
     if (username && password) {
@@ -9,6 +10,7 @@ async function loginFormHandler(event) {
         method: "post",
         body: JSON.stringify({
           username,
+          email,
           password,
         }),
         headers: { "Content-Type": "application/json" },
